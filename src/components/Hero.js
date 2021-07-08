94e8b01b9d84a3fab21a53246868a332e17d679c
MarcosMediano <marcosmediano@gmail.com>
2021-07-08 09:38:45 -0600

    Changed Hero structure and added props

---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./style/Hero.css"
+
+const Hero = (props) => {
+    return (
+        <React.Fragment>
+            <div className="Hero">
+                <div className="Hero__img" style={{ height: props.h }}>
+                    <img src={props.foto} alt="" />
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Hero;
