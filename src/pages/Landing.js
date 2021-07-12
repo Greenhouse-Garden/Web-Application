f76d11d4fbae96f287a66df0e990b39dd104e521
MarcosMediano <marcosmediano@gmail.com>
2021-07-12 04:45:09 -0600

    Added clase props to Button in Landing.js

---
+++ src/pages/Landing.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Hero from "../components/Hero"
+import Button from "../components/MainButton"
+import "./style/Landing.css"
+
+const Landing=(props) =>{
+    return(
+        <React.Fragment>
+            <div className="Main">
+                <div className="Main__content">
+                    <h1 className="Main__title">{props.title || "Greenhouse"}</h1>
+                    <h2 className="Main__subtitle">{props.subtitle || "Garden"}</h2>
+                    <Button clase={"darkButton"} contentText={"Get your own Greenhouse"} link={"/new"} id={"landingButton"}></Button>
+                </div>
+                <Hero h={"88vh"} foto={"https://images.pexels.com/photos/2749165/pexels-photo-2749165.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}></Hero>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Landing;
