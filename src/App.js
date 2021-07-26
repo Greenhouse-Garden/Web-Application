93d6e562c755b5cc548f6378449e6bdcbd7ddf03
MarcosMediano <marcosmediano@gmail.com>
2021-07-26 14:23:17 -0600

    Added routes for Sign Up and New product pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Landing from "./pages/Landing";
 import Layout from "./pages/Layout"
 import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import NewProduct from "./pages/NewProduct"
 
 
 function App() {
@@ -13,7 +15,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={Landing}></Route>
           <Route exact path="/signin" component={SignIn}></Route>
-          
+          <Route exact path="/signup" component={SignUp}></Route>
+          <Route exact path="/newproduct" component={NewProduct}></Route>
           
         </Switch>
       </Layout>
