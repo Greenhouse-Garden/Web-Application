c421679888f4acdabf7c2793fa87e1acf29a75b3
MarcosMediano <marcosmediano@gmail.com>
2021-07-26 18:33:48 -0600

    Created Product Info component files

---
+++ src/components/ProductInfo.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react"
+import './style/ProductInfo.css'
+
+class ProductInfo extends Component{
+    render(){
+        return(
+            <div className="container">
+
+            </div>
+        );
+    }
+}
+
+export default ProductInfo;
