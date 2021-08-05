c3a69aee5c16ded0a522ac7d9e6588f7d0590aa8
MarcosMediano <marcosmediano@gmail.com>
2021-08-05 15:27:26 -0600

    Delete title and subtitle props

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -8,8 +8,8 @@ const Landing=(props) =>{
         <React.Fragment>
             <div className="Main">
                 <div className="Main__content">
-                    <h1 className="Main__title">{props.title || "Greenhouse"}</h1>
-                    <h2 className="Main__subtitle">{props.subtitle || "Garden"}</h2>
+                    <h1 className="Main__title">Greenhouse</h1>
+                    <h2 className="Main__subtitle">Garden</h2>
                     <Button clase={"darkButton"} contentText={"Get your own Greenhouse"} link={"/new"} id={"landingButton"}></Button>
                 </div>
                 <Hero h={"88vh"} foto={"https://images.pexels.com/photos/2749165/pexels-photo-2749165.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}></Hero>
