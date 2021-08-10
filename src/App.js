c7bf6218bdc2bcce6f0f8091c16a1c59d5b28ff0
Angel-Davila <a6519150005@utch.edu.mx>
2021-08-10 02:47:53 -0600

    any reference to signup use that may cause an error deleted

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import Landing from "./pages/Landing";
 import Layout from "./pages/Layout"
 import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
 import NewProduct from "./pages/NewProduct"
 import Product from "./pages/Product"
 import Catalogue from "./pages/Catalogue"
@@ -21,7 +20,6 @@ function App() {
           <Switch>
             <Route exact path="/" component={Landing}></Route>
             <Route exact path="/signin/" component={SignIn}></Route>
-            <Route exact path="/signup/" component={SignUp}></Route>
             <Route exact path="/newproduct/" component={NewProduct}></Route>
             <Route exact path="/product/" component={Product}></Route>
             <Route exact path="/catalogue/" component={Catalogue}></Route>
