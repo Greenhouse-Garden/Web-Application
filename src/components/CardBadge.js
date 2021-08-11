6f853c8e1fccd9790856698a26141bbb3de2e6e7
Angel-Davila <a6519150005@utch.edu.mx>
2021-08-11 07:35:46 -0600

    wrong import

--- src/components/CardBadge.js
+++ src/components/CardBadge.js
@@ -6,7 +6,6 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
-import Button2 from "../components/MainButton"
 import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom'
 
