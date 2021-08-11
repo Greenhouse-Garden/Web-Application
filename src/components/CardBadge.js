f00a34365ad6be2ff762ae9c60ba275e39b63c87
MarcosMediano <marcosmediano@gmail.com>
2021-08-11 05:24:24 -0600

    Changed button background color

--- src/components/CardBadge.js
+++ src/components/CardBadge.js
@@ -7,18 +7,24 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
+import Button2 from "../components/MainButton"
 import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom'
 
 const useStyles = makeStyles({
     root: {
         maxWidth: 280,
         margin: 50,
-        maxHeight: 600,
-        minHeight: 375,
+        maxHeight: 400,
+        minHeight: 400,
 
     },
-    image:{
+    image: {
+        minHeight: 200,
         maxHeight: 200
+    },
+    boton: {
+        backgroundColor: "#8ba752"
     }
 });
 
@@ -45,9 +51,9 @@ const CardBadge = (props) => {
                 <CardActions>
                     <div className="row">
                         <div className="col-md-6">
-                            <Button onClick={"/product"} variant="outlined" size="small" color="black">
+                            <Button component={Link} to={`/product/${props.id}`} className={classes.boton} variant="outlined" size="small" color="black">
                                 See product
-                            </Button>
+                            </Button> 
                         </div>
                         <div className="col-md-6 m-auto">
                             <Typography gutterBottom variant="h5" component="h4">
