bc5eff970fc008865fcde9a6d8eeadb11e3d5187
Angel-Davila <a6519150005@utch.edu.mx>
2021-08-12 13:12:09 -0600

    logout button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React from "react"
 import "./style/Header.css"
-import { IoCart } from "react-icons/io5"
-import Button from "../components/MainButton"
+
 
 
 class Header extends React.Component {
@@ -10,7 +9,7 @@ class Header extends React.Component {
             <React.Fragment>
                 <div className="Header">
                     <a href = "/"><img src="https://media.discordapp.net/attachments/844617951055577128/874570232713342986/Greenhouse_logo.png?width=676&height=676" alt="header_image" /></a>
-                    <a href = "/cart"><img src="https://media.discordapp.net/attachments/844617951055577128/874994305809383444/cartIcon.png" /></a>
+                    <a href = "/cart"><img src="https://media.discordapp.net/attachments/844617951055577128/874994305809383444/cartIcon.png" alt=""/></a>
                 </div>
             </React.Fragment>
         );
