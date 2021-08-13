c31e8ff42908866703ef7641aded34e0de4fa824
Angel-Davila <a6519150005@utch.edu.mx>
2021-08-12 22:59:42 -0600

    verification page

---
+++ src/pages/Verification.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+
+class Verification extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>You account has been verificated</h1>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Verification;
