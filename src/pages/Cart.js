db7d43f361b855429df14f03283cb83ee7d8611f
Angel-Davila <a6519150005@utch.edu.mx>
2021-08-13 02:27:15 -0600

    products to cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -18,14 +18,24 @@ const Cart = (props) => {
                         <div className="row">
                             <div className="col-md-12">
                                 <CartProduct
+                                    price={"4"}
+                                    name={"Eggplant"}
+                                    image={"https://cdn.shopify.com/s/files/1/0260/4702/1102/products/5fcbd3fd6a9c03601e4251c8_2c01105e-4ece-48cf-9b85-652f6aa8a843_1024x1024.png?v=1613494238"}
+                                ></CartProduct>
+                                <CartProduct
                                     price={"3"}
-                                    name={"Asparagus 50 seeds package"}
-                                    image={"https://images-ext-2.discordapp.net/external/R0EP15Z9HIvI3hFRWlluO-DUCWhJFX8otOyjX6GRsJg/%3Fv%3D1614818781/https/cdn.shopify.com/s/files/1/0260/4702/1102/products/5fdbee7326d53134fea9465c_ee199281-49c0-49b7-b555-d3dcd200bde0_1024x1024.png"}
+                                    name={"Cherry Tomato"}
+                                    image={"https://cdn.shopify.com/s/files/1/0260/4702/1102/products/5fc8b984b511930a7c7e8da1_1024x1024.png?v=1615668358"}
                                 ></CartProduct>
                                 <CartProduct
                                     price={"2"}
-                                    name={"Tomato seeds 50 seeds package"}
-                                    image={"https://images.pexels.com/photos/1327838/pexels-photo-1327838.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}
+                                    name={"Carrot"}
+                                    image={"https://cdn.shopify.com/s/files/1/0260/4702/1102/products/5f37bae7616b9b050368e41b_8b55225c-32b0-4b9a-b4ac-d324db8fc9e1_1024x1024.jpg?v=1604248491"}
+                                ></CartProduct>
+                                <CartProduct
+                                    price={"2"}
+                                    name={"White Onion"}
+                                    image={"https://cdn.shopify.com/s/files/1/0260/4702/1102/products/5fc2e4956997a438fa8cf885_1024x1024.png?v=1615743652"}
                                 ></CartProduct>
                             </div>
                         </div>
